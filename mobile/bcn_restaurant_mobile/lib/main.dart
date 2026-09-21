810964ab6c5836a3cb1e6ddfc031f7a8359eed67
HtayOoLwin <htayoo.lwin@bcncl.com>
2026-09-21 14:36:56 +0630

    fix: do not block app startup on logo decoding

--- mobile/bcn_restaurant_mobile/lib/main.dart
+++ mobile/bcn_restaurant_mobile/lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
-import 'core/branding/doh_myot_daw_logo.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await preloadDohMyotDawLogo();
   runApp(const ProviderScope(child: BcnRestaurantApp()));
 }
